858a54c007302c3431a16e2fc97989ed8ab49a9f
r0ute <route-a@ya.ru>
2018-10-30 18:04:08 +0300

    split simulations

---
+++ src/gatling/simulations/com/example/nonblocking/BlockingSimulation.scala
@@ -0,0 +1,19 @@
+package com.example.nonblocking
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class BlockingSimulation extends Simulation {
+
+  val httpConf = http.baseUrl("http://localhost:8080")
+
+  val scn = scenario("Non-blocking Simulation")
+    .exec(http("non-blocking-request")
+      .get("/non-blocking")
+      .check(status.is(200))
+    )
+
+  setUp(
+    scn.inject(rampUsersPerSec(50).to(200).during(120))
+  ).protocols(httpConf)
+}
---
+++ src/gatling/simulations/com/example/nonblocking/NonBlockingSimulation.scala
@@ -0,0 +1,19 @@
+package com.example.nonblocking
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class NonBlockingSimulation extends Simulation {
+
+  val httpConf = http.baseUrl("http://localhost:8080")
+
+  val scn = scenario("Blocking Simulation")
+    .exec(http("blocking-request")
+      .get("/blocking")
+      .check(status.is(200))
+    )
+
+  setUp(
+    scn.inject(rampUsersPerSec(50).to(200).during(120))
+  ).protocols(httpConf)
+}
